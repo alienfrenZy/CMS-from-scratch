0f1d7441836602491cb85e22fd770beff3a7fecb
Adrian Marikar <adrianmarikar@gmail.com>
2016-08-26 21:06:00 +0100

    Add files via upload

---
+++ CMS from scratch/admin/categories.php
@@ -0,0 +1,98 @@
+<!-- HEADER -->
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+<!-- NAVIGATION -->       
+<?php include "includes/admin_navigation.php";?>        
+       
+       
+<div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+                      
+                      <div class="col-xs-6">
+                      
+                      
+<?php
+
+    insert_categories();
+
+?>
+                      
+                      
+                      
+                      <form action="" method="post">
+                          <div class="form-group">
+                             <label for="cat_title">Add Category</label>                              
+                              <input type="text" class="form-control" name="cat_title">                              
+                          </div>
+                           <div class="form-group">                              
+                              <input class="btn btn-primary" type="submit" name="submit" value="Add Category">                              
+                          </div>
+                          
+                      </form>
+                       
+              
+
+<?php // UPDATE AND INCLUDE QUERY
+
+    updateCategories();   
+                          
+                          
+?>                      
+      
+                        </div><!--Add Category Form -->
+                        
+                        <div class="col-xs-6">
+                           
+
+                           
+                            <table class="table table-bordered table-hover">
+                                <thread>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thread>
+                                <tbody>
+                                   
+<?php // FIND ALL CATEGORIES QUERY
+
+    findAllCategories();
+
+?>
+                                                  
+<?php // DELETE QUERY
+                                    
+    deleteCategories();
+                    
+?>
+                                                   
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+   
+        </div>
+        
+        
+<!-- FOOTER -->            
+<?php include "includes/admin_footer.php"; ?>       
+       
+ 
